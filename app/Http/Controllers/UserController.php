4e12aa08ffecd63cd9e50310f51bc882af078b95
Pelumi <fasanyafemi@gmail.com>
2024-12-17 11:20:58 +0100

    updating users roles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -194,7 +194,7 @@ class UserController extends Controller
         } catch (\Exception $e) {
             return response()->json([
                 'status' => false,
-                'message' => 'User deletion failed',
+                'message' => 'User status not updated',
                 'error' => $e->getMessage(),
             ], 500);
         }
