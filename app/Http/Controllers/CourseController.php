c3df7e911f7002ba6879efb7376584f3b411592b
Pelumi <fasanyafemi@gmail.com>
2025-03-07 11:45:04 +0100

    content update

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -329,11 +329,11 @@ class CourseController extends Controller
 
             // Update course
             $course = Course::where('uuid', $courseId)->firstOrFail();
-            $courseData = json_decode($request->data, true);
-            return $courseData;
+            $courseData = json_decode($request->all()[0], true);
             $realdata = collect($courseData)->except(['sections', 'id', 'created_at', 'updated_at'])->toArray();
-            return [$course, $realdata];
             $course->update($realdata);
+
+            return $course;
             
             
             return $course;
