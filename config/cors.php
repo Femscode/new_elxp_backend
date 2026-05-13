ea60d1ae9cb5a93b6410c18384ec716a3003742a
Pelumi <fasanyafemi@gmail.com>
2026-05-13 15:18:54 +0100

    fixing cors issues

--- config/cors.php
+++ config/cors.php
@@ -23,6 +23,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'https://elxp-trainers.connectinskillz.com',
+        'https://elxp-learners.connectinskillz.com',
         'https://nifinspired.connectinskillz.com',
         'https://nifinspired.connectinskillz.com/api',
     ],
